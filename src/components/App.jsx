1c4beb6dfc0f377d4af7ff681d5f855da2e0a4f3
DanielaCode <danielagp96@gmail.com>
2022-08-02 09:49:57 -0500

    agrego configuraciones y correcciones, renderizo el componente App y corrijo el mode de webpack a development

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>
-            <h1>hola mundo!</h1>
-        </div>
+        <h1>hola mundo! dani</h1>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';  
-import ReactDOM from 'react-dom';   
+import {createRoot} from 'react-dom/client';   
+import App from './components/App';
 
-ReactDOM.render(<App/>,document.querySelector("#app"));
+const container = document.querySelector("#app");
+const root = createRoot(container);
+root.render(<App tab="home"/>);
